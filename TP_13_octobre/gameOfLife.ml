e1e9c5468df7bdf254ee7d60ff5ad10287a09ee4
joratora <adhiol73546@gmail.com>
2017-10-14 19:49:53 +0200

    1ere partie finie, 2eme en cours

--- TP_13_octobre/gameOfLife.ml
+++ TP_13_octobre/gameOfLife.ml
@@ -30,36 +30,42 @@ let is_alive cell = cell <> empty ;;
 (*******************   FROM TP 3 *********************)
 (*   insert here needed simple functions on lists    *)
 
+let draw_line list size col =
+  let rec dl l1 si i =
+    match list with
+      | []   -> ()
+      | e::q -> draw_cell (col, size * i) cell_size (cell_color (nth_int i list)); draw_line q size col
+  in
+  dl list size 0 ;;
+
+
+let rec nth_list i list =
+  match list with
+    | []   -> []
+    | e::q -> if i = 0 then e else nth_list (i-1) q ;;
+
+let rec nth_int i list =
+  match list with
+    | []   -> 0
+    | e::q -> if i = 0 then e else nth_int (i-1) q ;;
+
 let rec length liste =
    match liste with
      | []   -> 0
      | e::q -> 1 + length q ;;
 
-let rec count liste x a b =
-  match liste with
-    | []   -> 0
-    | e::q -> (if !(is_alive e) then 0 else 1) + count q x ;;
+let rec count_line liste a b =
+  match b with
+    | n when n = a -> nth_int a liste
+    | n            -> nth_int n liste + count_line liste a (n-1) ;;
 
-let nth i l =
-  if i < 0 || l = []
+
+let does_exist board (x, y) =
+  if x >= length board || x <0 || y < 0 || y >= length (nth_list x board)
   then
-    invalid_arg "nth: index has to be positive and list must not be empty"
+    0
   else
-    let rec nt k li =
-      match (k, li) with
-	| (_, [])   -> failwith "nth: element not found"
-	| (0, e::q) -> e
-	| (x, e::q) -> nt (x-1) q
-    in
-    nt i l ;;
-
-let search_pos x l =
-  let rec sp x l i =
-    match l with
-      | []   -> failwith "search_pos: not found "
-      | e::q -> if x = e then i else sp x q (i+1)
-  in
-  sp x l 0 ;;
+    1 ;;
 
 let rec init_list n x =
   match n with
@@ -73,6 +79,7 @@ let rec replace list i x =
 	             x::replace q (n-1) x
                    else
 	             e::replace q (n-1) x ;;
+
 	
 
 (*******************   Toolbox *********************)
@@ -83,33 +90,46 @@ let rec replace list i x =
 let rec gen_board (l, c) x =
   match l with
     | 0 -> []
-    | n -> (init_list c x)::gen_board (n-1, c) x ;; 
+    | n -> (init_list c x)::gen_board (n-1, c) x ;;
 
 (* get_cell *)
 
 let get_cell (x, y) board =
-  nth y (nth x board) ;;
+  nth_int y (nth_list x board) ;;
 
 (* put_cell *)
 
 let rec put_cell xx (x, y) board =
-  replace board x (replace (nth x board) y xx) ;;
+  if does_exist board (x, y) = 0
+  then
+    board
+  else
+    replace board x (replace (nth_list x board) y xx) ;;
 
 (* count_neighbours *)
 
-let rec count_neighbours (x, y) board (l, c) =
-  match l with
-    | 0
+let count_neighbours (x, y) board (l, c) =
+  let rec ctn a b =
+    match b with
+      | n when n = a -> does_exist board (x, y) * count_line (nth_list a (board)) (y-1) (y+1) - 1
+      | n            -> does_exist board (x, y) * count_line (nth_list n (board)) (y-1) (y+l) + ctn a (n-1)
+  in
+  ctn (x-1) (x+1) ;;
 
 (************************************************************)
 (*                  graphics                                *)
 (*        from the board to the graphic window              *)
+
+let draw_cell (x, y) size color =
+  set_color grey;
+  draw_rect (x+1) (y+1) size size;
+  set_color color; 
+  fill_rect (x+2) (y+2) (size-2) (size-2) ;;
+
+let draw_board board size =
 
 
 
 (************************************************************)
 (*                     Game of life                         *)
 (************************************************************)
-
-
-
